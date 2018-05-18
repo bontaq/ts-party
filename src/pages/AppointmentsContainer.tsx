1037a65409e5ad2cc2e73b8adc931581d55db7be
Ian Davidson <bontaq@gmail.com>
2018-05-18 07:54:29 -0400

    Continue reducing shame

--- src/pages/AppointmentsContainer.tsx
+++ src/pages/AppointmentsContainer.tsx
@@ -1,83 +1,85 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { State } from '../reducer';
-import { Appointment } from '../models';
-import { formatDate } from '../util';
-import Table from '../components/Table';
-import Modal from '../components/Modal';
-import { type } from 'os';
+import * as React from "react";
+import { connect } from "react-redux";
+import { State as ReducerState } from "../reducer";
+import { Appointment } from "../models";
+import { formatDate } from "../util";
+import Table from "../components/Table";
+import Modal from "../components/Modal";
+import { type } from "os";
 
+type Props = {
+  fetchAppointments: () => null;
+  appointments: Array<Appointment & { patient_name: string }>;
+};
 
-interface IAppointmentContainerProps {
-  fetchAppointments: () => null
-  appointments: Array<Appointment & { patient_name: string }>
-}
+type State = {
+  modalOpen: boolean;
+  selected?: Appointment & { patient_name: string };
+};
 
-type appointmentRow = [string, string, string]
+type appointmentRow = [string, string, string];
 
-interface IAppointmentContainerState {
-  modalOpen: boolean
-  selected?: Appointment & { patient_name: string }
-}
-
-class AppointmentsContainer extends React.Component<IAppointmentContainerProps, IAppointmentContainerState> {
-  state: IAppointmentContainerState = {
+class AppointmentsContainer extends React.Component<Props, State> {
+  state: State = {
     modalOpen: false,
-  }
+  };
 
   componentDidMount() {
-    this.props.fetchAppointments()
+    this.props.fetchAppointments();
   }
 
   handleClick = (d: Array<any>) => {
     const selected = this.props.appointments.filter(a => {
-      return a.id == d[0]
-    })
-    console.info('selected', selected);
+      return a.id == d[0];
+    });
     this.setState({
       modalOpen: true,
-      selected: selected[0]
+      selected: selected[0],
     });
-    console.info(this.state);
-  }
+  };
 
   render() {
     const appointments = this.props.appointments.map(
-      ({ id, patient_name, datetime }) => [id, patient_name, formatDate(datetime)]
-    )
-    const modal = this.state.modalOpen
-      ? <Modal
-        handleClose={() => this.setState({
-          modalOpen: false,
-          selected: null
-        })}
-        header={this.state.selected.patient_name}>
+      ({ id, patient_name, datetime }) => [
+        id,
+        patient_name,
+        formatDate(datetime),
+      ]
+    );
+    const modal = this.state.modalOpen ? (
+      <Modal
+        handleClose={() =>
+          this.setState({
+            modalOpen: false,
+            selected: null,
+          })
+        }
+        header={this.state.selected.patient_name}
+      >
         <p>Note: {this.state.selected.note}</p>
       </Modal>
-      : null;
+    ) : null;
     return (
       <div>
         {modal}
         <Table
           columnNames={["Id", "Patient Name", "Appointment Time"]}
           data={appointments}
-          onClick={(d) => this.handleClick(d)}
+          onClick={d => this.handleClick(d)}
         />
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state: State) => {
-  return {
-    appointments: state.appointments
-  }
-}
+const mapStateToProps = (state: ReducerState) => ({
+  appointments: state.appointments,
+});
 
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    fetchAppointments: () => dispatch({ type: 'FETCH_APPOINTMENTS' })
-  }
-}
+const mapDispatchToProps = (dispatch: any) => ({
+  fetchAppointments: () => dispatch({ type: "FETCH_APPOINTMENTS" }),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(AppointmentsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  AppointmentsContainer
+);
